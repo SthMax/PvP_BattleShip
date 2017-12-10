a9f850bb861bb470506ba914257ae4e00f87d613
SthMax <alanmax@me.com>
2017-12-10 01:12:38 -0600

    test

--- src/gameMain.java
+++ src/gameMain.java
@@ -27,7 +27,7 @@ public class gameMain {
 
             String keyboardInput = s.nextLine();
             commandSwitcher(keyboardInput.toLowerCase());
-
+            //testchanges
         }
     }
 
